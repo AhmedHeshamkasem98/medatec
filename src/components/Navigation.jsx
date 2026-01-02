1fd1cff4649f63a289118253ded3cf747d515053
Ahmed Hesham <151786369+AhmedHeshamkasem98@users.noreply.github.com>
2026-01-03 00:52:02 +0200

    Adjust logo height in Navigation component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -27,7 +27,7 @@ export default function Navigation({ activeSection, onSectionChange }) {
           <img
             src={logo}
             alt="MedaTech"
-            className="h-22 w-auto transition-transform hover:scale-110"
+            className="h-20 w-auto transition-transform hover:scale-110"
           />
 
           {/* Desktop Menu */}
